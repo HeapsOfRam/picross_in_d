a9d5ae520d2ee244be36d1f70aba1961ff22104b
Ryan March <rmarch03@villanova.edu>
2016-02-13 13:33:21 -0800

    adding sequence counter to calculate hint numbers
    An integral part of the Picross puzzle game are the hint numbers. These
    numbers let the player know about contiguous 'true' or 'filled'
    sequences within a row or column. In order to adhere to that
    functionality, a contiguous sequence counter has been added for both
    rows and columns. This will track hint numbers in arrays respective to
    their particular dimensions.
    
    This is an initial and sloppy implementation. Future enhancements will
    be made. Encapsulation will be considered (weighing the pros and cons of
    such a method). Asynchronous array traversal will also be considered.
    Finally, if possible it would be wonderful to remove any column-major
    array traversal in order to fully optimize performance (not a necessity,
    but it is a nice hands-on learning tool for performance focused
    methodologies).

--- puzzle_maker.d
+++ puzzle_maker.d
@@ -12,6 +12,8 @@ void main()
     write(">> ");
     readf(" %s", &y_dimension);
 
+    writeln();
+
     bool[][] puzzle_board;
 
     for(int x = 0; x < x_dimension; ++x)
@@ -27,6 +29,97 @@ void main()
         puzzle_board ~= puzzle_line;
     }
 
+    int[][] x_sequences;
 
-    writeln(puzzle_board);
+    for(int x = 0; x < x_dimension; ++x)
+    {
+        int[] slice_sequences;
+        int single_sequence = 0;
+        bool should_add_end = true;
+
+        for(int y = 0; y < y_dimension; ++y)
+        {
+            if(puzzle_board[x][y] == true)
+            {
+                ++single_sequence;
+                should_add_end = true;
+            }
+            else
+            {
+                if(single_sequence > 0)
+                {
+                    slice_sequences ~= single_sequence;
+                    single_sequence = 0;
+                    should_add_end = false;
+                }
+            }
+        }
+
+        if(should_add_end)
+        {
+            slice_sequences ~= single_sequence;
+        }
+
+        x_sequences ~= slice_sequences;
+    }
+
+    int[][] y_sequences;
+
+    for(int y = 0; y <  y_dimension; ++y)
+    {
+        int[] slice_sequences;
+        int single_sequence = 0;
+        bool should_add_end = true;
+
+        for(int x = 0; x < x_dimension; x++)
+        {
+            if(puzzle_board[x][y] == true)
+            {
+                ++single_sequence;
+                should_add_end = true;
+                //writeln("sequence++");
+                //writeln("%d, %d", x, y);
+            }
+            else
+            {
+                if(single_sequence > 0)
+                {
+                    slice_sequences ~= single_sequence;
+                    single_sequence = 0;
+                    should_add_end = false;
+
+                    //writeln("sequence append");
+                    //writeln("%d, %d", x, y);
+                }
+            }
+        }
+
+        if(should_add_end)
+        {
+            slice_sequences ~= single_sequence;
+        }
+
+        y_sequences ~= slice_sequences;
+    }
+
+    writeln("---X---");
+    for(int x = 0; x < x_sequences.length; ++x)
+    {
+        writeln(x_sequences[x]);
+    }
+
+
+    writeln("---Y---");
+    for(int y = 0; y < y_sequences.length; ++y)
+    {
+        writeln(y_sequences[y]);
+    }
+
+    writeln();
+    writeln("******");
+
+    for(int x = 0; x < x_dimension; ++x)
+    {
+        writeln(puzzle_board[x]);
+    }
 }
