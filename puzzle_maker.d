5dec13cde7603909e4abb23303c6fa52fa417051
Ryan March <rmarch03@villanova.edu>
2016-02-13 12:33:59 -0800

    randomly generate array of booleans for dimensions
    Puzzle maker now takes in input for both the x dimension and the y
    dimension for the puzzle board. The program will then make a
    2-Dimensional array based on those constraints and populate with
    randomized booleans.
    
    This is a first step into arrays and random. Will look into better
    boolean randomization and even asynchronous array population.
    
    Also added initial files to gitignore.

--- puzzle_maker.d
+++ puzzle_maker.d
@@ -1,6 +1,32 @@
 import std.stdio;
+import std.random;
 
 void main()
 {
-    writeln("hello, world!");
+    int x_dimension;
+    int y_dimension;
+
+    write(">> ");
+    readf(" %s", &x_dimension);
+
+    write(">> ");
+    readf(" %s", &y_dimension);
+
+    bool[][] puzzle_board;
+
+    for(int x = 0; x < x_dimension; ++x)
+    {
+        bool[] puzzle_line;
+
+        for(int y = 0; y < y_dimension; ++y)
+        {
+            auto random_num = uniform(0, 720);
+            puzzle_line ~= (random_num % 2 == 0);
+        }
+
+        puzzle_board ~= puzzle_line;
+    }
+
+
+    writeln(puzzle_board);
 }
